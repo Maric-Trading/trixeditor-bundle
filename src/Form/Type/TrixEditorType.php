d5ddab295564768bc418a8b81b470cfbe7154f8a
Eric <eric.robert@simpleclinic.net>
2023-05-18 17:52:24 +1000

    Adds option to allow uploads, disabled by default

--- src/Form/Type/TrixEditorType.php
+++ src/Form/Type/TrixEditorType.php
@@ -5,8 +5,10 @@ namespace MaricTrading\TrixeditorBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TrixEditorType extends AbstractType
 {
@@ -15,6 +17,23 @@ class TrixEditorType extends AbstractType
     {
         //return FormType::class;
         return TextareaType::class;
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults([
+            "allow_uploads"=>false,
+        ]);
+    }
+
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+        $view->vars['allow_uploads'] = $options['allow_uploads'] ?? false;
     }
 
 
